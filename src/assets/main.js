1b2bff012e2feab6467d49415bd2b1ef716e8cc4
crdx <pub@crdx.org>
2023-10-08 18:04:37 +0100

    Remove unused JavaScript and whitespace

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,33 +1,11 @@
-function findParentByClass(node, className) {
-  let current = node
-
-  while (current !== null) {
-    if (current.classList && current.classList.contains(className)) {
-      return current
-    }
-    current = current.parentNode
-  }
-
-  return null
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     const burgers = document.querySelectorAll('.navbar-burger') || []
 
-    burgers.forEach(function(el) {
+    burgers.forEach(el => {
         el.addEventListener('click', function() {
             const target = document.getElementById(el.dataset.target)
             el.classList.toggle('is-active')
             target.classList.toggle('is-active')
-        })
-    });
-
-    const deleteButtons = document.querySelectorAll('.notification .delete') || []
-
-    deleteButtons.forEach(function(deleteNode) {
-        deleteNode.addEventListener('click', function() {
-            const notification = findParentByClass(deleteNode, 'container')
-            notification.parentNode.removeChild(notification)
         })
     })
 })
