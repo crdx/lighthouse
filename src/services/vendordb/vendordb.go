a1ac3c07025db3cc1ed70c1609777ecfe1d5c456
crdx <pub@crdx.org>
2023-10-10 19:14:46 +0100

    Move vendor lookup code into its own method
    The various `continue`s littered throughout the loop were bypassing the
    time.Sleep at the end, thus hitting the rate limit. By moving it to its
    own method we can more conveniently use a defer call to ensure that the
    sleep is guaranteed to execute, without complicating the code.

--- src/services/vendordb/vendordb.go
+++ src/services/vendordb/vendordb.go
@@ -23,6 +23,8 @@ func New() *VendorDB {
 	return &VendorDB{}
 }
 
+const backoff = 10 * time.Second
+
 func (self *VendorDB) Init(args *services.Args) error {
 	self.log = args.Logger
 
@@ -43,62 +45,69 @@ func (self *VendorDB) Run() error {
 
 		log := self.log.With("adapter_id", adapter.ID, "mac", adapter.MACAddress)
 
-		update := func(vendor string) {
-			log.Info("lookup complete", "vendor", vendor)
-
-			var succeeded bool
-			if vendor != "" {
-				adapter.Update("vendor", vendor)
-				succeeded = true
-			}
-
-			lookup.Update("processed", true, "succeeded", succeeded)
+		if err := doLookup(lookup, adapter, log); err != nil {
+			return err
 		}
-
-	retry:
-
-		res, err := self.getVendor(adapter.MACAddress)
-
-		if err != nil || res.StatusCode == http.StatusNotFound {
-			update("")
-			continue
-		}
-
-		if res.StatusCode == http.StatusUnauthorized {
-			log.Error("authorisation failed", "response_code", res.StatusCode, "body", res.String())
-			return errors.New("authorisation failed")
-		}
-
-		if res.StatusCode == http.StatusTooManyRequests {
-			delay := 5 * time.Second
-			self.log.Info("throttling", "response_code", res.StatusCode, "delay", delay)
-			time.Sleep(delay)
-
-			goto retry
-		}
-
-		if res.StatusCode != http.StatusOK {
-			log.Error("request failed", "response_code", res.StatusCode, "body", res.String())
-			continue
-		}
-
-		vendor := res.String()
-
-		if vendor == "" || res.StatusCode == http.StatusNotFound {
-			update("")
-			continue
-		}
-
-		update(vendor)
-
-		// MacVendors API free plan allows 2 requests per second, so to be safe limit to 1 per second.
-		time.Sleep(time.Second)
 	}
 
 	return nil
 }
 
-func (*VendorDB) getVendor(macAddress string) (*req.Response, error) {
+func doLookup(lookup *m.VendorLookup, adapter *m.Adapter, log *slog.Logger) error {
+	defer func() {
+		// MacVendors API free plan allows 2 requests per second, so to be safe limit to 1 per second.
+		time.Sleep(time.Second)
+	}()
+
+	update := func(vendor string) {
+		log.Info("lookup complete", "vendor", vendor)
+
+		var succeeded bool
+		if vendor != "" {
+			adapter.Update("vendor", vendor)
+			succeeded = true
+		}
+
+		lookup.Update("processed", true, "succeeded", succeeded)
+	}
+
+retry:
+
+	res, err := getVendor(adapter.MACAddress)
+
+	if err != nil || res.StatusCode == http.StatusNotFound {
+		update("")
+		return nil
+	}
+
+	if res.StatusCode == http.StatusUnauthorized {
+		log.Error("authorisation failed", "response_code", res.StatusCode, "body", res.String())
+		return errors.New("authorisation failed")
+	}
+
+	if res.StatusCode == http.StatusTooManyRequests {
+		log.Info("throttling", "response_code", res.StatusCode, "delay", backoff)
+		time.Sleep(backoff)
+		goto retry
+	}
+
+	if res.StatusCode != http.StatusOK {
+		log.Error("request failed", "response_code", res.StatusCode, "body", res.String())
+		return errors.New("request failed")
+	}
+
+	vendor := res.String()
+
+	if vendor == "" || res.StatusCode == http.StatusNotFound {
+		update("")
+		return nil
+	}
+
+	update(vendor)
+	return nil
+}
+
+func getVendor(macAddress string) (*req.Response, error) {
 	return req.C().R().
 		SetBearerAuthToken(env.MACVendorsAPIKey).
 		SetHeader("Accept", "text/plain").
