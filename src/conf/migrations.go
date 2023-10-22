e1be08ae0264fe07ba5d27c77d4fe3995455db8f
crdx <pub@crdx.org>
2023-10-22 10:51:08 +0100

    Delete migration

--- src/conf/migrations.go
+++ src/conf/migrations.go
@@ -2,19 +2,14 @@ package conf
 
 import (
 	"github.com/go-gormigrate/gormigrate/v2"
-	"gorm.io/gorm"
 )
 
 var migrations = []*gormigrate.Migration{
-	{
-		ID: "1697905074_InsertDefaultTimeZone",
-		Migrate: func(db *gorm.DB) error {
-			return db.Exec(`
-				INSERT INTO settings
-					(name, value, created_at, updated_at)
-				VALUES
-					('timezone', 'Europe/London', UTC_TIMESTAMP(), UTC_TIMESTAMP())
-			`).Error
-		},
-	},
+	// {
+	// 	ID: "XXX",
+	// 	Migrate: func(db *gorm.DB) error {
+	// 		return db.Exec(`
+	// 		`).Error
+	// 	},
+	// },
 }
