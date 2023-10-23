00a89d7dcdc9c071c8774c90e6883b5293fd2d18
crdx <pub@crdx.org>
2023-10-23 20:52:52 +0100

    Dynamically associate labels with form fields

--- src/assets/main.js
+++ src/assets/main.js
@@ -72,3 +72,36 @@ function initForms() {
         })
     })
 }
+
+function uuid() {
+    const a = new Uint8Array(16)
+    crypto.getRandomValues(a)
+
+    a[6] = (a[6] & 0x0f) | 0x40 // v4
+    a[8] = (a[8] & 0x3f) | 0x80
+
+    const segments = [
+        a.subarray(0, 4),
+        a.subarray(4, 6),
+        a.subarray(6, 8),
+        a.subarray(8, 10),
+        a.subarray(10, 16),
+    ]
+
+    const f = bytes => [...bytes].map(b => b.toString(16).padStart(2, '0')).join('')
+    return segments.map(f).join('')
+}
+
+const id = (function() {
+    // Save CPU cycles by reusing the same UUID across multiple calls to id(), yielding unique id
+    // values that start with an alpha character.
+    let prefix
+    let counter = 1
+
+    return function() {
+        if (!prefix) {
+            prefix = uuid()
+        }
+        return 'i_' + prefix + '_' + counter++
+    }
+})()
